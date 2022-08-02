e31b18c3c51266f3eb92e77722d3d216719e7b82
dmitry <dbufistov@ntent.com>
2022-08-02 18:31:33 +0200

    Fixed path.

--- scripts/drop_cassandra_tables.sh
+++ scripts/drop_cassandra_tables.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-docker exec -it autocompletedemo-cassandra cqlsh --request-timeout 60 -f /app/src/main/resources/drop_tables.cqlsh
+docker exec -it autocompletedemo-cassandra cqlsh --request-timeout 60 -f /resources/drop_tables.cqlsh
